ce447ca86cad398eee02f34a56add1434fae0209
gissandrogama <gissandrogama@gmail.com>
2021-01-22 12:18:38 -0300

    gigalixir CD process

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+echo "Start Gigalixir Deploy"
+git remote add gigalixir https://henrymonteirogama%40gmail.com:f360c255-2d5d-4ea2-8ff9-b6e0cc84f202@git.gigalixir.com/order.git
+git push gigalixir main
+echo "Finish Gigalixir Deploy"
