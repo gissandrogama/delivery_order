4397bed06296d7525bb92a092d1b96583d8cd9d1
gissandrogama <gissandrogama@gmail.com>
2021-01-28 02:57:43 -0300

    created parse controller e route parse

--- lib/order_api_web/controllers/parse_controller.ex
+++ lib/order_api_web/controllers/parse_controller.ex
@@ -1,18 +1,19 @@
 defmodule OrderApiWeb.ParseController do
   use OrderApiWeb, :controller
+  alias OrderApi.Services.Solicit
 
   def index(conn, _params) do
-    OrderApi.Services.Solicit.run()
+    Solicit.run()
     |> case do
       {:ok, response} ->
         conn
         |> put_status(:ok)
         |> json(%{message: Jason.decode!(response.request.body)})
 
-      {:error, reason} ->
+      {:error, _reason} ->
         conn
         |> put_status(:bad_request)
-        |> json(%{message: reason})
+        |> json(%{message: "Parse cannot be sent"})
     end
   end
 end
